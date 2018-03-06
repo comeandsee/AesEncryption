50c0ab2795e42e9e028d542b984b470447c0180e
Monika Ruszczyk <ruszczyk.monika@gmail.com>
2018-03-06 10:01:09 +0100

    added select file button

--- BSKprojekt1/MainWindow.xaml.cs
+++ BSKprojekt1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,16 @@ namespace BSKprojekt1
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
 
+        }
+
+        private void SelectInputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //InputFileTextBox.
+            }
         }
     }
 }
